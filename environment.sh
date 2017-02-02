a2781f735a670bd6874424db4be17a6f474dcc3c
KOSZÓ, Norbert <k.norbi@gmail.com>
2017-02-02 21:45:55 +0100

    Speed it up a little bit

--- environment.sh
+++ environment.sh
@@ -3,5 +3,6 @@ export SDK_BASE=/home/norbi/projektek/esp8266/esp-open-sdk/sdk
 export ESPTOOL=/home/norbi/projektek/esp8266/esp-open-sdk/esptool/esptool.py
 export ESPPORT=/dev/ttyUSB0
 export ESPBAUD=115200
+export ESPBAUD=460800
 export USE_OPENSDK=yes
 
