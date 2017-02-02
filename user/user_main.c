e40635282d40024ef3c46f54e859b4c6e6c74a01
KOSZÓ, Norbert <k.norbi@gmail.com>
2017-02-02 21:46:27 +0100

    Somewhat working websocket + topnav

--- user/user_main.c
+++ user/user_main.c
@@ -48,36 +48,30 @@ int myPassFn(HttpdConnData *connData, int no, char *user, int userLen, char *pas
 	return 0;
 }
 
-void wsLedRecv(Websock *ws, char *data, int len, int flags) {
-/*     int i;
-       char buff[128];
-       os_sprintf(buff, "You sent: ");
-       for (i=0; i<len; i++) buff[i+10]=data[i];
-       buff[i+10]=0;
-    cgiWebsocketSend(ws, buff, os_strlen(buff), WEBSOCK_FLAG_NONE);*/
+
+void ICACHE_FLASH_ATTR getLedStatusJson(char *buff)
+{
+    uint8_t ledStatus = ioGetLed();
+    os_sprintf(buff, "{\"led\":\"%s\"}", (ledStatus) ? "on" : "off" );
 }
-
-
-//Broadcast the uptime in seconds every second over connected websockets
-//static void ICACHE_FLASH_ATTR websockTimerCb(void *arg) {
-//	static int ctr=0;
-//	char buff[128];
-//	ctr++;
-//	os_sprintf(buff, "Up for %d minutes %d seconds!\n", ctr/60, ctr%60);
-//	cgiWebsockBroadcast("/websocket/ws.cgi", buff, os_strlen(buff), WEBSOCK_FLAG_NONE);
-//}
 
 void ICACHE_FLASH_ATTR sendLedStatus()
 {
     char buff[128];
-    uint8_t ledStatus = ioGetLed();
-    os_sprintf(buff, "{\"led\":\"%s\"}", (ledStatus) ? "on" : "off" );
+    getLedStatusJson(buff);
     cgiWebsockBroadcast("/led-ws.cgi", buff, os_strlen(buff), WEBSOCK_FLAG_NONE);
+}
+
+
+void wsLedRecv(Websock *ws, char *data, int len, int flags) {
+    char buff[128];
+    getLedStatusJson(buff);
+    cgiWebsocketSend(ws, buff, strlen(buff), WEBSOCK_FLAG_NONE);
 }
 
 void wsLedConnect(Websock *ws) {
     ws->recvCb=wsLedRecv;
-    sendLedStatus();
+//    sendLedStatus();
 }
 
 #ifdef ESPFS_POS
