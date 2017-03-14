918a6f89d805970b1ab2dd395dcb71eeb590617c
KOSZÓ, Norbert <k.norbi@gmail.com>
2017-03-14 22:25:48 +0100

    Add some basic test framework for the html page

---
+++ test/template.sed
@@ -0,0 +1,1 @@
+s/%devicename%/testDevice/g
---
+++ test/test-html.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+THIS_SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
+HTML_SOURCE_DIR="${THIS_SCRIPT_DIR}/../html"
+CGI_SOURCE_DIR="${THIS_SCRIPT_DIR}/cgi-src"
+HTML_TARGET_DIR="${THIS_SCRIPT_DIR}/html"
+SED_TEMPLATE="${SED_TEMPLATE-"${THIS_SCRIPT_DIR}/template.sed"}"
+
+rm -rf "${HTML_TARGET_DIR}"
+mkdir -p "${HTML_TARGET_DIR}"
+
+cp -r "${HTML_SOURCE_DIR}/"* "${HTML_TARGET_DIR}"
+cp -r "${CGI_SOURCE_DIR}/"* "${HTML_TARGET_DIR}"
+find "${HTML_TARGET_DIR}" -name "*.tpl" -exec sed -f "${SED_TEMPLATE}" -i {} \;
+find "${HTML_TARGET_DIR}" -name "*.tpl" -exec rename 's/tpl$/html/' {} \;
+
+
+pushd "${HTML_TARGET_DIR}"
+python -m SimpleHTTPServer 8080 &
+SERVER_PID=$!
+trap "kill ${SERVER_PID}" SIGINT SIGTERM
+popd
+
+inotifywait --format %w%f ${HTML_SOURCE_DIR} -r -m -e close_write | while read source; do 
+    target="${HTML_TARGET_DIR}${source#${HTML_SOURCE_DIR}}"
+    if [[ "${source}" =~ .*\.tpl ]]; then
+        echo Rendering template ${source} to ${target%.*}.html
+        sed -f "${SED_TEMPLATE}" ${source} > ${target%.*}.html
+    else
+        echo Copying ${source} to ${target}
+        cp ${source} ${target}
+    fi
+done
