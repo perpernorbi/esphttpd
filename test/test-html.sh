4b26d1c4a95fc8224ceb2db67a764177a3f066d5
KOSZÓ, Norbert <k.norbi@gmail.com>
2017-03-15 20:57:42 +0100

    Fix watch + render js
    qtcreator actually moves the files, instead of writing them.

--- test/test-html.sh
+++ test/test-html.sh
@@ -12,6 +12,7 @@ mkdir -p "${HTML_TARGET_DIR}"
 cp -r "${HTML_SOURCE_DIR}/"* "${HTML_TARGET_DIR}"
 cp -r "${CGI_SOURCE_DIR}/"* "${HTML_TARGET_DIR}"
 find "${HTML_TARGET_DIR}" -name "*.tpl" -exec sed -f "${SED_TEMPLATE}" -i {} \;
+find "${HTML_TARGET_DIR}" -name "*.js" -exec sed -f "${SED_TEMPLATE}" -i {} \;
 find "${HTML_TARGET_DIR}" -name "*.tpl" -exec rename 's/tpl$/html/' {} \;
 
 
@@ -21,13 +22,17 @@ SERVER_PID=$!
 trap "kill ${SERVER_PID}" SIGINT SIGTERM
 popd
 
-inotifywait --format %w%f ${HTML_SOURCE_DIR} -r -m -e close_write | while read source; do 
+inotifywait --format %w%f ${HTML_SOURCE_DIR} -r -m -e close_write,move | while read source; do 
     target="${HTML_TARGET_DIR}${source#${HTML_SOURCE_DIR}}"
     if [[ "${source}" =~ .*\.tpl ]]; then
         echo Rendering template ${source} to ${target%.*}.html
         sed -f "${SED_TEMPLATE}" ${source} > ${target%.*}.html
-    else
-        echo Copying ${source} to ${target}
-        cp ${source} ${target}
+    else if [[ "${source}" =~ .*\.js ]]; then
+            echo Rendering javascript ${source} to ${target}
+            sed -f "${SED_TEMPLATE}" ${source} > ${target}
+        else
+            echo Copying ${source} to ${target}
+            cp ${source} ${target}
+        fi
     fi
 done
