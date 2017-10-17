1aff6a7a9f2f6a1cdd2a9ec74e362630befa1b6e
KOSZÓ, Norbert <k.norbi@gmail.com>
2017-10-17 17:14:46 +0200

    Fix indentation

--- user/user_main.c
+++ user/user_main.c
@@ -1,14 +1,14 @@
 /*
  * ----------------------------------------------------------------------------
  * "THE BEER-WARE LICENSE" (Revision 42):
- * Jeroen Domburg <jeroen@spritesmods.com> wrote this file. As long as you retain 
- * this notice you can do whatever you want with this stuff. If we meet some day, 
- * and you think this stuff is worth it, you can buy me a beer in return. 
+ * Jeroen Domburg <jeroen@spritesmods.com> wrote this file. As long as you retain
+ * this notice you can do whatever you want with this stuff. If we meet some day,
+ * and you think this stuff is worth it, you can buy me a beer in return.
  * ----------------------------------------------------------------------------
  */
 
 /*
-This is example code for the esphttpd library. It's a small-ish demo showing off 
+This is example code for the esphttpd library. It's a small-ish demo showing off
 the server, including WiFi connection management capabilities, some IO and
 some pictures of cats.
 */
@@ -36,17 +36,17 @@ some pictures of cats.
 //This is disabled in the default build; if you want to try it, enable the authBasic line in
 //the builtInUrls below.
 int myPassFn(HttpdConnData *connData, int no, char *user, int userLen, char *pass, int passLen) {
-	if (no==0) {
-		os_strcpy(user, "admin");
-		os_strcpy(pass, "s3cr3t");
-		return 1;
+    if (no==0) {
+        os_strcpy(user, "admin");
+        os_strcpy(pass, "s3cr3t");
+        return 1;
 //Add more users this way. Check against incrementing no for each user added.
 //	} else if (no==1) {
 //		os_strcpy(user, "user1");
 //		os_strcpy(pass, "something");
 //		return 1;
-	}
-	return 0;
+    }
+    return 0;
 }
 
 int ICACHE_FLASH_ATTR cgiWiFiStatus (HttpdConnData *connData)
@@ -116,20 +116,20 @@ void wsLedConnect(Websock *ws) {
 
 #ifdef ESPFS_POS
 CgiUploadFlashDef uploadParams={
-	.type=CGIFLASH_TYPE_ESPFS,
-	.fw1Pos=ESPFS_POS,
-	.fw2Pos=0,
-	.fwSize=ESPFS_SIZE,
+    .type=CGIFLASH_TYPE_ESPFS,
+    .fw1Pos=ESPFS_POS,
+    .fw2Pos=0,
+    .fwSize=ESPFS_SIZE,
 };
 #define INCLUDE_FLASH_FNS
 #endif
 #ifdef OTA_FLASH_SIZE_K
 CgiUploadFlashDef uploadParams={
-	.type=CGIFLASH_TYPE_FW,
-	.fw1Pos=0x1000,
-	.fw2Pos=((OTA_FLASH_SIZE_K*1024)/2)+0x1000,
-	.fwSize=((OTA_FLASH_SIZE_K*1024)/2)-0x1000,
-	.tagName=OTA_TAGNAME
+    .type=CGIFLASH_TYPE_FW,
+    .fw1Pos=0x1000,
+    .fw2Pos=((OTA_FLASH_SIZE_K*1024)/2)+0x1000,
+    .fwSize=((OTA_FLASH_SIZE_K*1024)/2)-0x1000,
+    .tagName=OTA_TAGNAME
 };
 #define INCLUDE_FLASH_FNS
 #endif
@@ -145,38 +145,38 @@ general ones. Authorization things (like authBasic) act as a 'barrier' and
 should be placed above the URLs they protect.
 */
 HttpdBuiltInUrl builtInUrls[]={
-	{"*", cgiRedirectApClientToHostname, "esp8266.nonet"},
-	{"/", cgiRedirect, "/index.tpl"},
+    {"*", cgiRedirectApClientToHostname, "esp8266.nonet"},
+    {"/", cgiRedirect, "/index.tpl"},
     {"/index.tpl", cgiEspFsTemplate, tplIndex},
-	{"/led.cgi", cgiLed, NULL},
+    {"/led.cgi", cgiLed, NULL},
 #ifdef INCLUDE_FLASH_FNS
-	{"/flash/next", cgiGetFirmwareNext, &uploadParams},
-	{"/flash/upload", cgiUploadFirmware, &uploadParams},
+    {"/flash/next", cgiGetFirmwareNext, &uploadParams},
+    {"/flash/upload", cgiUploadFirmware, &uploadParams},
 #endif
-	{"/flash/reboot", cgiRebootFirmware, NULL},
+    {"/flash/reboot", cgiRebootFirmware, NULL},
 
-	//Routines to make the /wifi URL and everything beneath it work.
+    //Routines to make the /wifi URL and everything beneath it work.
 
 //Enable the line below to protect the WiFi configuration with an username/password combo.
 //	{"/wifi/*", authBasic, myPassFn},
 
-	{"/wifi", cgiRedirect, "/wifi/wifi.tpl"},
-	{"/wifi/", cgiRedirect, "/wifi/wifi.tpl"},
-	{"/wifi/wifiscan.cgi", cgiWiFiScan, NULL},
-	{"/wifi/wifi.tpl", cgiEspFsTemplate, tplWlan},
-	{"/wifi/connect.cgi", cgiWiFiConnect, NULL},
-	{"/wifi/connstatus.cgi", cgiWiFiConnStatus, NULL},
+    {"/wifi", cgiRedirect, "/wifi/wifi.tpl"},
+    {"/wifi/", cgiRedirect, "/wifi/wifi.tpl"},
+    {"/wifi/wifiscan.cgi", cgiWiFiScan, NULL},
+    {"/wifi/wifi.tpl", cgiEspFsTemplate, tplWlan},
+    {"/wifi/connect.cgi", cgiWiFiConnect, NULL},
+    {"/wifi/connstatus.cgi", cgiWiFiConnStatus, NULL},
     {"/wifi/wifistatus.cgi", cgiWiFiStatus, NULL},
-	{"/wifi/setmode.cgi", cgiWiFiSetMode, NULL},
+    {"/wifi/setmode.cgi", cgiWiFiSetMode, NULL},
 
     {"/led-ws.cgi", cgiWebsocket, wsLedConnect},
 
-	{"/test", cgiRedirect, "/test/index.html"},
-	{"/test/", cgiRedirect, "/test/index.html"},
-	{"/test/test.cgi", cgiTestbed, NULL},
+    {"/test", cgiRedirect, "/test/index.html"},
+    {"/test/", cgiRedirect, "/test/index.html"},
+    {"/test/test.cgi", cgiTestbed, NULL},
 
-	{"*", cgiEspFsHook, NULL}, //Catch-all cgi function for the filesystem
-	{NULL, NULL, NULL}
+    {"*", cgiEspFsHook, NULL}, //Catch-all cgi function for the filesystem
+    {NULL, NULL, NULL}
 };
 
 
@@ -184,25 +184,25 @@ HttpdBuiltInUrl builtInUrls[]={
 static ETSTimer prHeapTimer;
 
 static void ICACHE_FLASH_ATTR prHeapTimerCb(void *arg) {
-	os_printf("Heap: %ld\n", (unsigned long)system_get_free_heap_size());
+    os_printf("Heap: %ld\n", (unsigned long)system_get_free_heap_size());
 }
 #endif
 
 //Main routine. Initialize stdout, the I/O, filesystem and the webserver and we're done.
 void user_init(void) {
-	stdoutInit();
-	ioInit();
+    stdoutInit();
+    ioInit();
     ioLedChangeHandler(sendLedStatus);
-	captdnsInit();
+    captdnsInit();
 
-	// 0x40200000 is the base address for spi flash memory mapping, ESPFS_POS is the position
-	// where image is written in flash that is defined in Makefile.
+    // 0x40200000 is the base address for spi flash memory mapping, ESPFS_POS is the position
+    // where image is written in flash that is defined in Makefile.
 #ifdef ESPFS_POS
-	espFsInit((void*)(0x40200000 + ESPFS_POS));
+    espFsInit((void*)(0x40200000 + ESPFS_POS));
 #else
-	espFsInit((void*)(webpages_espfs_start));
+    espFsInit((void*)(webpages_espfs_start));
 #endif
-	httpdInit(builtInUrls, 80);
+    httpdInit(builtInUrls, 80);
 #ifdef SHOW_HEAP_USE
     //Norbi
     //os_timer_disarm(&prHeapTimer);
@@ -213,9 +213,9 @@ void user_init(void) {
     //os_timer_disarm(&websockTimer);
     //os_timer_setfn(&websockTimer, websockTimerCb, NULL);
     //os_timer_arm(&websockTimer, 1000, 1);
-	os_printf("\nReady\n");
+    os_printf("\nReady\n");
 }
 
 void user_rf_pre_init() {
-	//Not needed, but some SDK versions want this defined.
+    //Not needed, but some SDK versions want this defined.
 }
